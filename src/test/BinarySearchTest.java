4fcdd887fca55125415ad591c7815a6ff70c0ab7
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2023-11-02 14:47:29 +0000

    Set up JUnit testing

---
+++ src/test/BinarySearchTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+    
+    @Test
+    public void smth(){
+        assertEquals(0, 0);
+    }
+}

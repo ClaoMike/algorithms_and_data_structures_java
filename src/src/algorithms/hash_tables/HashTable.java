4ba13e2704adb32a7206021fb6e48b6db32b9db5
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2023-11-05 23:04:53 +0000

    Some more comments

--- src/src/algorithms/hash_tables/HashTable.java
+++ src/src/algorithms/hash_tables/HashTable.java
@@ -50,7 +50,7 @@ public class HashTable {
         // if it's not a duplicate, then it counts
         UNAVAILABLE_SLOTS++;
 
-        // before adding, see if the table needs to extended, so it does not exceed the optimal load factor - usually 0.7
+        // before adding, see if the table needs to extended, so it does not exceed the maximum load factor - usually 0.7
         float loadFactor = calculateLoadFactor();
 
         if(loadFactor >= LOAD_FACTOR_THRESHOLD_UPPER_BOUND) {
@@ -65,18 +65,22 @@ public class HashTable {
         if(table[hashValue] != null) {
             KVP lastLink = table[hashValue];
 
+            // find the last item in list, it does not have any kids
             while (lastLink.kid != null) {
                 lastLink = lastLink.kid;
             }
 
+            // attach new item and make it knowledgable about its parent
             lastLink.kid = newPair;
             newPair.parent = lastLink;
         }
-        else {
-            newPair.parent = newPair;
+        // if there is no collision, then he is the one that becomes the top of a linked list
+        else {  
+            newPair.parent = newPair; // he is aware that he is the first one, by being its own parent (don't ask questions!)
             table[hashValue] = newPair;
         }
 
+        // let the user know the insertion was successful
         return true;
     }
 
@@ -84,61 +88,74 @@ public class HashTable {
         // calculate index using the hash function
         int searchIndex = calculateHashValue(key, table.length);
 
-        // if the value is null, means it's not there, return null
+        // if there is nothing in the table at the hash position, then it's not here
         if(table[searchIndex] == null) {
             return null;
         }
-        // otherwise, it's there, return the pair
+        // otherwise, search it through the linked list
         else {
             KVP lastLink = table[searchIndex];
 
+            // there is at least one pair saved at this location, we know that
             do {
 
+                // if keys match, it's a return
                 if(lastLink.KEY.equals(key)) {
                     return lastLink;
                 } 
+                // keep searching through the linked list
                 else {
                     lastLink = lastLink.kid;
                 }
-
+            // search 'till the end
             } while(lastLink != null);
 
+            // still not found? then it's not here
             return null;
         }
     }
 
     public boolean delete(String key) {
-        // otherwise, delete it and update the number of occupied slots
+        // find it
         KVP toBeDeleted = search(key);
 
+        // not here? cannot be deleted then
         if(toBeDeleted == null) {
             return false;
         }
 
+        // at this point, it can be deleted, uncount it
         UNAVAILABLE_SLOTS--;
 
+        // if it's the first one in the linked list, then move its kid in its place
         if(toBeDeleted.parent == toBeDeleted) {
             int hashValue = calculateHashValue(key, table.length);
             table[hashValue] = toBeDeleted.kid;
             
+            // there is a slight chance that his kid might be null - happens
+            // however, if it's not null, then it's time for hime to become his own parent
             if(table[hashValue] != null) {
                 table[hashValue].parent = table[hashValue];
             }
         }
+        // if it's not the first one, it could be in the middle of the linked list, let his parent know that he is going somewhere else - Java cemetery
         else {
             toBeDeleted.parent.kid = toBeDeleted.kid;
 
+            // before leaving this realm, let its kids (if any) who their new parent is
             if(toBeDeleted.kid != null) {
                 toBeDeleted.kid.parent = toBeDeleted.parent;
             }
         }
 
+        // before leaving, check that the load factor is above the miminum load factor - usuallt 0.4
         float loadFactor = calculateLoadFactor();
 
         if(loadFactor < LOAD_FACTOR_THRESHOLD_LOWER_BOUND && table.length/2 >= originalSize) {
             resize(Resize.DECREASE);
         }
 
+        // let the user know that the operation was a success
         return true;
     }
 
@@ -147,6 +164,7 @@ public class HashTable {
 
         if(type == Resize.INCREASE) {
             newTable = new KVP[table.length * 2];
+            // or DECREASE
         } else {
             newTable = new KVP[table.length / 2];
         }
